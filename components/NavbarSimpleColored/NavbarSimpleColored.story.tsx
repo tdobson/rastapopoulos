65adaf528704bf7e13247fb4d4fa0ac18be1520c
tdobson <hello@tdobson.net>
2024-07-23 15:19:06 +0100

    added all components

--- components/NavbarSimpleColored/NavbarSimpleColored.story.tsx
+++
@@ -1,9 +0,0 @@
-import attributes from './attributes.json';
-import { StoryWrapper } from '../../components/StoryWrapper/StoryWrapper';
-import { NavbarSimpleColored } from './NavbarSimpleColored';
-
-export default { title: 'NavbarSimpleColored' };
-
-export function Usage() {
-  return <StoryWrapper attributes={attributes} component={NavbarSimpleColored} />;
-}
