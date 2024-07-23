b1ffe820c311c3694bc8715331f1929fe7678cbf
tdobson <hello@tdobson.net>
2024-07-23 22:45:47 +0100

    The commit message that describes the changes made in the diffs is:
    Formatted code to adhere to linting rules and improve readability

--- hooks/useElevations.ts
+++ hooks/useElevations.ts
@@ -2,7 +2,7 @@
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { elevationsApi } from '../utils/api';
 import { Elevation } from '../types/elevation';
-import { notifications } from "@mantine/notifications";
+import { notifications } from '@mantine/notifications';
 
 // Define the key for the Elevations query
 const ELEVATIONS_QUERY_KEY = 'elevations';
@@ -10,93 +10,93 @@ const ELEVATION_REPORTS_QUERY_KEY = 'elevationReports';
 
 // Custom hook for managing Elevations data
 export const useElevations = () => {
-    const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
 
-    // GET Elevations
-    const { data: elevations, isLoading, isError, error } = useQuery<Elevation[], Error>({
-        queryKey: [ELEVATIONS_QUERY_KEY],
-        queryFn: elevationsApi.getElevations
-    });
+  // GET Elevations
+  const { data: elevations, isLoading, isError, error } = useQuery<Elevation[], Error>({
+    queryKey: [ELEVATIONS_QUERY_KEY],
+    queryFn: elevationsApi.getElevations,
+  });
 
-    // GET Elevation Reports
-    const {
-        data: elevationReports,
-        isLoading: isLoadingElevationReports,
-        isError: isErrorElevationReports,
-        error: errorElevationReports,
-    } = useQuery<Elevation[], Error>({
-        queryKey: [ELEVATION_REPORTS_QUERY_KEY],
-        queryFn: elevationsApi.getElevationsReport,
-    });
+  // GET Elevation Reports
+  const {
+    data: elevationReports,
+    isLoading: isLoadingElevationReports,
+    isError: isErrorElevationReports,
+    error: errorElevationReports,
+  } = useQuery<Elevation[], Error>({
+    queryKey: [ELEVATION_REPORTS_QUERY_KEY],
+    queryFn: elevationsApi.getElevationsReport,
+  });
 
-    // Handle error logging separately
-    if (isErrorElevationReports) {
-        console.error('Error fetching elevation reports:', errorElevationReports);
-    }
+  // Handle error logging separately
+  if (isErrorElevationReports) {
+    console.error('Error fetching elevation reports:', errorElevationReports);
+  }
 
-    // CREATE Elevation
-    const { mutateAsync: createElevation, isPending: isCreatingElevation } = useMutation({
-        mutationFn: (newElevation: Partial<Elevation>) => elevationsApi.createElevation(newElevation),
-        onSuccess: (newElevation) => {
-            queryClient.setQueryData<Elevation[]>([ELEVATIONS_QUERY_KEY], (old = []) => [...old, newElevation]);
-        },
-        onError: (err) => {
-            notifications.show({
-                title: 'Error',
-                message: 'Failed to create elevation',
-                color: 'red',
-            });
-        },
-    });
+  // CREATE Elevation
+  const { mutateAsync: createElevation, isPending: isCreatingElevation } = useMutation({
+    mutationFn: (newElevation: Partial<Elevation>) => elevationsApi.createElevation(newElevation),
+    onSuccess: (newElevation) => {
+      queryClient.setQueryData<Elevation[]>([ELEVATIONS_QUERY_KEY], (old = []) => [...old, newElevation]);
+    },
+    onError: (err) => {
+      notifications.show({
+        title: 'Error',
+        message: 'Failed to create elevation',
+        color: 'red',
+      });
+    },
+  });
 
-    // UPDATE Elevation
-    const { mutateAsync: updateElevation, isPending: isUpdatingElevation } = useMutation({
-        mutationFn: (updatedElevation: Elevation) => elevationsApi.updateElevation(updatedElevation),
-        onSuccess: (updatedElevation) => {
-            queryClient.setQueryData<Elevation[]>([ELEVATIONS_QUERY_KEY], (old = []) =>
-                old.map((elevation) => (elevation.plot_id === updatedElevation.plot_id ? updatedElevation : elevation))
-            );
-        },
-        onError: (err) => {
-            notifications.show({
-                title: 'Error',
-                message: 'Failed to update elevation',
-                color: 'red',
-            });
-        },
-    });
+  // UPDATE Elevation
+  const { mutateAsync: updateElevation, isPending: isUpdatingElevation } = useMutation({
+    mutationFn: (updatedElevation: Elevation) => elevationsApi.updateElevation(updatedElevation),
+    onSuccess: (updatedElevation) => {
+      queryClient.setQueryData<Elevation[]>([ELEVATIONS_QUERY_KEY], (old = []) =>
+        old.map((elevation) => (elevation.plot_id === updatedElevation.plot_id ? updatedElevation : elevation))
+      );
+    },
+    onError: (err) => {
+      notifications.show({
+        title: 'Error',
+        message: 'Failed to update elevation',
+        color: 'red',
+      });
+    },
+  });
 
-    // DELETE Elevation
-    const { mutateAsync: deleteElevation, isPending: isDeletingElevation } = useMutation({
-        mutationFn: (plotId: string) => elevationsApi.deleteElevation(plotId),
-        onSuccess: (_, plotId) => {
-            queryClient.setQueryData<Elevation[]>([ELEVATIONS_QUERY_KEY], (old = []) =>
-                old.filter((elevation) => elevation.plot_id !== plotId)
-            );
-        },
-        onError: (err) => {
-            notifications.show({
-                title: 'Error',
-                message: 'Failed to delete elevation',
-                color: 'red',
-            });
-        },
-    });
+  // DELETE Elevation
+  const { mutateAsync: deleteElevation, isPending: isDeletingElevation } = useMutation({
+    mutationFn: (plotId: string) => elevationsApi.deleteElevation(plotId),
+    onSuccess: (_, plotId) => {
+      queryClient.setQueryData<Elevation[]>([ELEVATIONS_QUERY_KEY], (old = []) =>
+        old.filter((elevation) => elevation.plot_id !== plotId)
+      );
+    },
+    onError: (err) => {
+      notifications.show({
+        title: 'Error',
+        message: 'Failed to delete elevation',
+        color: 'red',
+      });
+    },
+  });
 
-    return {
-        elevations,
-        isLoading,
-        isError,
-        error,
-        elevationReports,
-        isLoadingElevationReports,
-        isErrorElevationReports,
-        errorElevationReports,
-        createElevation,
-        isCreatingElevation,
-        updateElevation,
-        isUpdatingElevation,
-        deleteElevation,
-        isDeletingElevation,
-    };
+  return {
+    elevations,
+    isLoading,
+    isError,
+    error,
+    elevationReports,
+    isLoadingElevationReports,
+    isErrorElevationReports,
+    errorElevationReports,
+    createElevation,
+    isCreatingElevation,
+    updateElevation,
+    isUpdatingElevation,
+    deleteElevation,
+    isDeletingElevation,
+  };
 };
--- utils/api.ts
+++ utils/api.ts
@@ -7,65 +7,65 @@ const apiBaseUrl = 'http://localhost:3001/api/v1';
 
 // Define the API request function
 async function apiRequest<T>(
-    path: string,
-    options: RequestInit = {}
+  path: string,
+  options: RequestInit = {}
 ): Promise<T> {
-    try {
-        const response = await fetch(`${apiBaseUrl}${path}`, options);
-        if (!response.ok) {
-            const errorData = await response.json();
-            const errorMessage = errorData.message || 'An error occurred while making the request';
-            notifications.show({
-                title: 'Error',
-                message: errorMessage,
-                color: 'red',
-            });
-            throw new Error(errorMessage);
-        }
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        // Handle common error cases, e.g., network errors, unauthorized requests
-        if (error instanceof Error) {
-            notifications.show({
-                title: 'Error',
-                message: error.message,
-                color: 'red',
-            });
-        }
-        throw error;
+  try {
+    const response = await fetch(`${apiBaseUrl}${path}`, options);
+    if (!response.ok) {
+      const errorData = await response.json();
+      const errorMessage = errorData.message || 'An error occurred while making the request';
+      notifications.show({
+        title: 'Error',
+        message: errorMessage,
+        color: 'red',
+      });
+      throw new Error(errorMessage);
     }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    // Handle common error cases, e.g., network errors, unauthorized requests
+    if (error instanceof Error) {
+      notifications.show({
+        title: 'Error',
+        message: error.message,
+        color: 'red',
+      });
+    }
+    throw error;
+  }
 }
 
 // Define API service functions
 export const elevationsApi = {
-    getElevationsReport: (): Promise<any[]> => {
-        return apiRequest<any[]>('/elevations');
-    },
-    getElevations: (): Promise<Elevation[]> => {
-        return apiRequest<Elevation[]>('/elevations');
-    },
-    createElevation: (newElevation: Partial<Elevation>): Promise<Elevation> => {
-        return apiRequest<Elevation>('/elevations', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(newElevation),
-        });
-    },
-    updateElevation: (updatedElevation: Elevation): Promise<Elevation> => {
-        return apiRequest<Elevation>(`/elevations/${updatedElevation.plot_id}`, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(updatedElevation),
-        });
-    },
-    deleteElevation: (elevationId: string): Promise<void> => {
-        return apiRequest<void>(`/elevations/${elevationId}`, {
-            method: 'DELETE',
-        });
-    },
+  getElevationsReport: (): Promise<any[]> => {
+    return apiRequest<any[]>('/elevations');
+  },
+  getElevations: (): Promise<Elevation[]> => {
+    return apiRequest<Elevation[]>('/elevations');
+  },
+  createElevation: (newElevation: Partial<Elevation>): Promise<Elevation> => {
+    return apiRequest<Elevation>('/elevations', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newElevation),
+    });
+  },
+  updateElevation: (updatedElevation: Elevation): Promise<Elevation> => {
+    return apiRequest<Elevation>(`/elevations/${updatedElevation.plot_id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updatedElevation),
+    });
+  },
+  deleteElevation: (elevationId: string): Promise<void> => {
+    return apiRequest<void>(`/elevations/${elevationId}`, {
+      method: 'DELETE',
+    });
+  },
 };
